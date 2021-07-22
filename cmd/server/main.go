e3b85df2b8f56a69b1e91dbf0973fd5e88e425b2
rino_pk <rino@pk.com>
2021-07-22 17:51:04 +0900

    myslqなしはうごく

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"GoAndMySQL/cmd/test"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	_ "github.com/go-sql-driver/mysql"
-	// go import の影響で消える
-	_ "github.com/hrdtknr/GoAndMySQL"
+	// _ "github.com/go-sql-driver/mysql"
+	// // go import の影響で消える
+	// _ "github.com/hrdtknr/GoAndMySQL"
 )
 
 func main() {
 
-	db := test.OpenDB(os.Getenv("DRIVER"), os.Getenv("DSN"))
-	defer test.CloseDB(db)
+	// db := test.OpenDB(os.Getenv("DRIVER"), os.Getenv("DSN"))
+	// defer test.CloseDB(db)
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("db.Ping failed:", err)
-	}
+	// if err := db.Ping(); err != nil {
+	// 	log.Fatal("db.Ping failed:", err)
+	// }
 
 	fmt.Println("start")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, "hello, docker container")
+		_, _ = fmt.Fprint(w, "hello, docker container\n")
 	})
 
 	// ローカルだと.env読めてない
